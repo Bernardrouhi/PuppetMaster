0a0e69c68da07f210628872b74006d28c920aa9e
Bernard Rouhi <bernardrouhi@gmail.com>
2020-01-30 00:48:51 +0800

    ready for release

---
+++ puppetMaster/widgets/CreateTemplateDialog.py
@@ -0,0 +1,69 @@
+import os, json
+from PySide2.QtWidgets import (QLabel, QGridLayout, QLineEdit,
+                          QPushButton, QDialog, QComboBox)
+from PySide2.QtCore import Qt
+from ..core.qnodes import (CommandType, PIIButton)
+from ..core.env_handler import (get_PMTemplateDir)
+
+class TemplateDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(TemplateDialog, self).__init__(*args, **kwargs)
+        self.initUI()
+
+    def initUI(self):
+        mainLayout = QGridLayout(self)
+        mainLayout.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        mainLayout.setColumnStretch(0,0)
+        mainLayout.setColumnStretch(1,1)
+        mainLayout.setColumnStretch(2,1)
+        self.setLayout(mainLayout)
+
+        line = 0
+        nameText = QLabel('Name:')
+        nameText.setAlignment(Qt.AlignRight)
+        self.nameIn = QLineEdit()
+        mainLayout.addWidget(nameText, line, 0)
+        mainLayout.addWidget(self.nameIn, line, 1, 1, 2)
+
+        line += 1
+        tempText = QLabel('Template:')
+        tempText.setAlignment(Qt.AlignRight)
+        self.tempCombo = QComboBox()
+        self.tempCombo.addItem('[NO TEMPLATE]',0)
+        self._read_templates()
+        mainLayout.addWidget(tempText, line, 0)
+        mainLayout.addWidget(self.tempCombo, line, 1, 1, 2)
+
+        line += 1
+        okBtn = QPushButton("Create")
+        okBtn.clicked.connect(self.accept)
+        cancelBtn = QPushButton("Cancel")
+        cancelBtn.clicked.connect(self.reject)
+
+        mainLayout.addWidget(okBtn, line, 1, 1, 1)
+        mainLayout.addWidget(cancelBtn, line, 2, 1, 1)
+        self.setWindowTitle("New Picker Interface Information")
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+    def _read_templates(self):
+        template_dir = get_PMTemplateDir()
+        if os.path.exists(template_dir):
+            files = os.walk(template_dir).next()[2]
+            for each in files:
+                if each.lower().endswith(".pii"):
+                    self.tempCombo.addItem(each, each)
+
+    def get_raw(self):
+        name = self.nameIn.text()
+        template_dir = get_PMTemplateDir()
+        template = self.tempCombo.currentText()
+        data = {}
+        if template != "[NO TEMPLATE]":
+            path = os.path.join(template_dir, template)
+            with open(path, 'r') as outfile:
+                data = json.load(outfile)
+        return {
+            'name':name,
+            'data': data
+        }
+    Raw = property(get_raw)
---
+++ puppetMaster/widgets/ParametersWidget.py
@@ -0,0 +1,160 @@
+from PySide2.QtWidgets import (QWidget, QHBoxLayout, QSizePolicy, 
+                          QLabel, QLineEdit, QComboBox)
+from PySide2.QtCore import (Qt, Signal)
+from PySide2.QtGui import (QColor)
+
+from ..core.qnodes import PickShape
+from ..core.icon import QIconSVG
+
+from .QPushColorButton import QPushColorButton, COLOR_PALETTE, BW_PALETTE
+
+class Parameters(QWidget):
+    onChangeBGColor = Signal(QColor)
+    onChangeFontColor = Signal(QColor)
+    onChangeFontSize = Signal(int)
+    onChangeText = Signal(str)
+    onChangeShape = Signal(str)
+    def __init__(self, parent=None):
+        super(Parameters, self).__init__(parent)
+        self._parent = parent
+
+        # self.setMinimumHeight(1)
+        self.setFixedHeight(35)
+        self.setMinimumWidth(1)
+
+        # ------------- Main Layout --------------
+        main_layout = QHBoxLayout(self)
+        main_layout.setContentsMargins(5,5,5,5)
+        main_layout.setSpacing(5)
+        main_layout.setAlignment(Qt.AlignTop)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        # ------- Row 1 -------
+        # ----------------------------------------
+        allLayout = QHBoxLayout()
+        allLayout.setContentsMargins(0,0,0,0)
+        allLayout.setSpacing(3)
+        allLayout.setAlignment(Qt.AlignTop|Qt.AlignLeft)
+
+        # Color
+        colorLabel = QLabel(u"Color:")
+        colorLabel.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.color_btn = QPushColorButton(columns=0,rows=6,palette=COLOR_PALETTE)
+        self.color_btn.colorSelected.connect(self.change_bg_color)
+
+        # Size
+        sizeLabel = QLabel(u"Size:")
+        sizeLabel.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.size_combo = QComboBox()
+        for i in range(1, 100):
+            self.size_combo.addItem(str(i),i)
+        self.size_combo.activated.connect(self.change_font_size)
+
+        # Shape
+        shapeLabel = QLabel(u"Shape:")
+        shapeLabel.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.shape_combo = QComboBox()
+        self.shape_combo.addItem(QIconSVG('square'),PickShape.SQUARE,PickShape.SQUARE)
+        self.shape_combo.addItem(QIconSVG('circle'),PickShape.CIRCLE,PickShape.CIRCLE)
+        self.shape_combo.addItem(QIconSVG('triangle'),PickShape.TRIANGLE,PickShape.TRIANGLE)
+        self.shape_combo.addItem(QIconSVG('plus'),PickShape.PLUS,PickShape.PLUS)
+        self.shape_combo.activated.connect(self.change_shape)
+
+        # Name
+        nameLabel = QLabel(u"Label:")
+        nameLabel.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        self.nameIn = QLineEdit()
+        self.nameIn.textEdited.connect(self.change_name)
+        self.textColor_btn = QPushColorButton(columns=1,rows=0,palette=BW_PALETTE)
+        self.textColor_btn.colorSelected.connect(self.change_font_color)
+
+        # Add widget to Layout
+        allLayout.addWidget(colorLabel)
+        allLayout.addWidget(self.color_btn)
+        allLayout.addWidget(sizeLabel)
+        allLayout.addWidget(self.size_combo)
+        allLayout.addWidget(shapeLabel)
+        allLayout.addWidget(self.shape_combo)
+        allLayout.addWidget(nameLabel)
+        allLayout.addWidget(self.nameIn)
+        allLayout.addWidget(self.textColor_btn)
+
+        main_layout.addLayout(allLayout)
+
+    def change_name(self):
+        '''
+        Sending Signal from "onChangeText" to change the text
+        '''
+        self.onChangeText.emit(self.nameIn.text())
+
+    def change_bg_color(self, color=QColor):
+        '''
+        Sending Signal from "onChangeBGColor" to change the
+        background color.
+
+        Parameters
+        ----------
+        color: (QColor)
+            QColor value to send over.
+        '''
+        self.onChangeBGColor.emit(color)
+
+    def change_font_color(self, color=QColor):
+        '''
+        Sending Signal from "onChangeFontColor" to change the
+        font color.
+
+        Parameters
+        ----------
+        color: (QColor)
+            QColor value to send over.
+        '''
+        self.onChangeFontColor.emit(color)
+
+    def change_font_size(self, value):
+        '''
+        Sending Signal from "onChangeFontSize" to change the
+        font size.
+        '''
+        self.onChangeFontSize.emit(value+1)
+
+    def change_shape(self):
+        '''
+        Sending Signal from "onChangeShape" to change the
+        shape of node.
+        '''
+        self.onChangeShape.emit(self.shape_combo.currentText())
+
+    def update_param(self, text=str, fontSize=int, fontColor=QColor, bgColor=QColor, shapeName=str()):
+        '''
+        Update the UI parameters.
+
+        Parameters
+        ----------
+        text: (str)
+            Name of the node.
+        fontSize: (int)
+            Size of the font.
+        fontColor: (QColor)
+            Color of the font.
+        bgColor: (QColor)
+            Color of the background.
+        '''
+        self.color_btn.CurrentColor = bgColor
+        self.textColor_btn.CurrentColor = fontColor
+        self.size_combo.setCurrentIndex(self.size_combo.findText(str(fontSize)))
+        self.nameIn.setText(text)
+        if shapeName:
+            self.shape_combo.setCurrentIndex(self.shape_combo.findText(shapeName))
+            self.shape_combo.setEnabled(True)
+        else:
+            self.shape_combo.setEnabled(False)
+            self.shape_combo.setCurrentIndex(-1)
+
+    def get_name(self):
+        return self.nameIn.text()
+    def set_name(self, value=str()):
+        self.nameIn.setText(value)
+    Name = property(get_name, set_name)
+
